80ba2ae99f1c8104ceab3d688a4d8fe38e43c819
Adam Oommen Jacob <adamrubiks@gmail.com>
2021-05-10 13:24:04 +0530

    first commit by adam

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,39 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Lmao(),
       ),
     );
+
+class Lmao extends StatefulWidget {
+
+  @override
+  _LmaoState createState() => _LmaoState();
+}
+
+class _LmaoState extends State<Lmao> {
+  var x=1;
+
+
+  @override
+  Widget build(BuildContext context) {
+    x=Random().nextInt(5)+1;
+    return Scaffold(
+backgroundColor: Colors.lightBlue,
+      appBar: AppBar(
+        title: Text('Ask Me Anything'),
+      ),
+      body: Center(
+      child: FlatButton(
+        child: Image.asset('images/ball$x.png'),onPressed: (){
+          setState(() {
+            x=Random().nextInt(5)+1;
+          });
+      },
+      )
+      ),
+    );
+  }
+}
